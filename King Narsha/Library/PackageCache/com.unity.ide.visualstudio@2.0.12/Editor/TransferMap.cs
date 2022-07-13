92245433df79c4566546739ccdda654f9981d3b8
Minji0219 <chlals0219@naver.com>
2022-07-13 11:44:53 +0900

    fadeout, fadein

---
+++ King Narsha/Library/PackageCache/com.unity.ide.visualstudio@2.0.12/Editor/TransferMap.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransferMap: MonoBehaviour
+{
+    public string transferMapName; //이동할 맵의 이름
+
+    //private MovingObject thePlayer; 
+
+    private FadeManager theFade;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //thePlayer = FindObjectType<MovingObject>();
+
+        theFade = FindObjectOfType<FadeManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //box에 닿으면 실행
+    {
+        if (collision.gameObject.name == "Player") ; //box에 닿은 객체가 플레이어라면
+        {
+            //thePlayer.currentMapName = transferMapName; //플레이어의 맵이름 
+            StartCoroutine(TransferCoroutine());
+        }
+    }
+
+    IEnumerator TransferCoroutine() 
+    {
+        theFade.FadeOut();//fadeout
+
+        yield return new WaitForSeconds(1f); //fadeout이후 대기
+
+
+        SceneManager.LoadScene(transferMapName); //맵이동 명령어
+
+        theFade.FadeIn();//fadein
+
+    }
+}
