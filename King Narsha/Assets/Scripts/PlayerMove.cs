691d92721f0590c0f010b594fa0112cf78abf994
Leeseoyeon12rl <bcily@ewhain.net>
2022-07-17 23:50:11 +0900

    wasd이동(수정x)플레이어카메라고정

---
+++ King Narsha/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+public class Control : MonoBehaviour
+{
+    // Update is called once per frame
+    int speed = 10; //스피드 
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //x축으로 이동할 양
+        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime; //y축으로 이동할양
+        this.transform.Translate(new Vector3(xMove, yMove, 0));  //이동
+
+    }
+}
