dda485c7f35ac1d88d8fa61d0f240c696782ea4e
Kang seungyeon <77188916+KangSYeon@users.noreply.github.com>
2022-07-21 01:10:32 +0900

    서연님 Merge
    맵 수정 및 wasd 이동, 카메라 추적까지 추가했습니다.

--- King Narsha/Assets/Scripts/PlayerMove.cs
+++ King Narsha/Assets/Scripts/PlayerMove.cs
@@ -1,14 +1,39 @@
-using UnityEngine;
 using System.Collections;
-public class Control : MonoBehaviour
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
 {
-    // Update is called once per frame
-    int speed = 10; //스피드 
+    [SerializeField]
+    float speed = 10.0f; //스피드
+    Vector2 _position;
+
+    void Start()
+    {
+        _position = transform.position;
+    }
+
     void Update()
     {
-        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //x축으로 이동할 양
-        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime; //y축으로 이동할양
-        this.transform.Translate(new Vector3(xMove, yMove, 0));  //이동
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _position += speed * Time.deltaTime * Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _position += speed * Time.deltaTime * Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _position += speed * Time.deltaTime * Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _position += speed * Time.deltaTime * Vector2.right;
+        }
+
+        transform.position = _position;
 
     }
 }
